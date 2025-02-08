637b0eb778470d9061f8ee304558e081c149550b
dana <d_yesserkey@kbtu.kz>
2025-02-08 14:53:44 +0500

    Create variables.js

---
+++ Lab3/javascript/Variables/variables.js
@@ -0,0 +1,16 @@
+let message;
+message = 'Hello!';
+
+alert(message);
+/*The multiline variant is a bit longer, but easier to read:*/
+let user = 'John';
+let age = 25;
+let message = 'Hello';
+/*Working with variables*/
+let admin, name;
+name = "John";
+admin = name;
+alert( admin );
+/* Giving the right name */
+let ourPlanetName = "Earth";
+let currentUserName = "John";
